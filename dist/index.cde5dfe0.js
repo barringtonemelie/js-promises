e3d341750daa6950a9d399e05ea23807efd40862
Emelie Barrington <emelie.barrington@chasacademy.se>
2023-01-31 20:21:05 +0100

    Finished the API calls, added error handling

--- dist/index.cde5dfe0.js
+++ dist/index.cde5dfe0.js
@@ -566,22 +566,29 @@ const timeWidget = document.querySelector(".time-widget");
 const creditWidget = document.querySelector(".credit-widget");
 const unsplashApi = ()=>{
     (0, _axiosDefault.default)("https://api.unsplash.com/photos/random/?client_id=CoDpo4M3ZKCnjLQDnieglH4aQMMoHBDG31eGuqJ4WMQ").then((response)=>{
-        console.log(response);
+        const photographerName = response.data.user.name;
+        const photographerUsername = response.data.user.username;
         bg.style.background = `center / cover no-repeat url(${response.data.urls.regular})`;
+        creditPhotographer(photographerName, photographerUsername);
     }).catch((error)=>{
         console.log(error);
+        //Backup background
+        bg.style.backgroundColor = "cadetblue";
     });
 };
-// unsplashApi(); 
-//Credit adress: response.data.user.name + länken till deras sida 
+function creditPhotographer(name, username) {
+    const creditHeader = document.querySelector(".credit-name");
+    const creditLink = document.querySelector(".credit-link");
+    const link = `https://unsplash.com/@${username}`;
+    creditLink.setAttribute("href", link);
+    creditHeader.innerText = name;
+}
+unsplashApi();
 function success(pos) {
     const crd = pos.coords;
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
     //Spara det returnerade värdet, skicka till en annan funktion som skriver ut det i index.html
-    getWeather(crd.latitude, crd.longitude);
-    getLocationName(crd.latitude, crd.longitude);
+    const currentWeather = getWeather(crd.latitude, crd.longitude);
+    const locationName = getLocationName(crd.latitude, crd.longitude);
 }
 function error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
@@ -589,25 +596,76 @@ function error(err) {
 navigator.geolocation.getCurrentPosition(success, error);
 async function getWeather(lat, long) {
     try {
-        const weatherData = await (0, _axiosDefault.default)(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=4810a4405a029b3e087478da7c24eba9`);
-        console.log(weatherData);
-    //returnera vädret 
+        const weatherData = await (0, _axiosDefault.default)(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=4810a4405a029b3e087478da7c24eba9`);
+        //returnera vädret 
+        const conditions = weatherData.data.weather[0].description;
+        const temp = Math.floor(weatherData.data.main.temp);
+        printWeather(temp, conditions);
     } catch (error) {
         console.error(error);
-    //Vad händer om vädret inte funkar? 
+        document.querySelector(".weather").innerText = "Unable to retrieve weather information";
     }
+}
+function printWeather(temp, conditions) {
+    const weatherHeader = document.querySelector(".weather");
+    weatherHeader.innerHTML = `${temp}°C </br> ${conditions}`;
 }
 async function getLocationName(lat, long) {
     try {
         const response = await (0, _axiosDefault.default)(`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&appid=4810a4405a029b3e087478da7c24eba9`);
-        console.log(response);
-        console.log("Current location: ", response.data[0].name);
-    //returnera plats-namnet 
+        const locationName = response.data[0].name;
+        printLocation(locationName);
     } catch (error) {
         console.error(error);
-    //Vad händer om platsen inte syns? 
+        document.querySelector(".location").innerText = "Unable to retrieve location";
     }
 }
+function printLocation(location) {
+    const locationHeader = document.querySelector(".location");
+    locationHeader.innerText = location;
+}
+async function kanyeQuotes() {
+    const kanyeQuote = document.querySelector(".kanye-quote");
+    try {
+        const quoteData = await (0, _axiosDefault.default)("https://api.kanye.rest");
+        kanyeQuote.innerText = `"${quoteData.data.quote}"`;
+        document.querySelector(".kanye-credit").innerText = "- Kanye";
+    } catch (error) {
+        console.error(error);
+        kanyeQuote.innerText = "Kanye didn't have anything to say today.";
+        document.querySelector(".kanye-credit").innerText = "- The Developer";
+    }
+}
+kanyeQuotes();
+async function boredActivities() {
+    const boredHeader = document.querySelector(".bored-activity");
+    try {
+        const response = await (0, _axiosDefault.default)("https://www.boredapi.com/api/activity");
+        boredHeader.innerText = response.data.activity;
+    } catch (error) {
+        console.error(error);
+        boredHeader.innerHTML = "Me too </br>- The Bored Developer";
+    }
+}
+boredActivities();
+const showDate = ()=>{
+    const currentDate = new Date().toJSON().slice(0, 10);
+    document.querySelector(".date").innerText = currentDate;
+};
+showDate();
+function showTime() {
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+    var time = h + ":" + m + ":" + s;
+    document.querySelector(".clock").innerText = time;
+    setTimeout(showTime, 1000);
+}
+showTime();
 
 },{"axios":"jo6P5","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"jo6P5":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
